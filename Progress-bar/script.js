051dcd44406e4fd6a94bb7b8948b18e1079ae4f3
Mehdi <moosorkh@gmail.com>
2022-07-10 14:27:05 -0700

    Expanding Cards

--- Progress-bar/script.js
+++ Progress-bar/script.js
@@ -1,44 +1,15 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle');
+const panels = document.querySelectorAll('.panel');
+console.log(panels[0]);
 
-let currentActive = 1;
-
-next.addEventListener('click', ()=>{
-    currentActive++;
-    if(currentActive > circles.length){
-        currentActive = circles.length;
-    }
-    update();
+panels.forEach((panel)=>{
+    panel.addEventListener('click', ()=>{
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
 })
 
-prev.addEventListener('click', ()=>{
-    currentActive--;
-    if(currentActive < 1){
-        currentActive = 1
-    }
-    update();
-})
-
-function update(){
-    circles.forEach((circle, idx)=>{
-        if(idx < currentActive){
-            circle.classList.add('active');
-        }else{
-            circle.classList.remove('active');
-        }
+function removeActiveClasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active');
     })
-
-    const actives = document.querySelectorAll('.active');
-   progress.style.width = (actives.length-1) / (circles.length -1) * 100 + '%';
-
-   if(currentActive === 1){
-        prev.diabled = true
-   }else if(currentActive === circles.length){
-    next.disabled = true;
-   }else{
-    prev.disabled = false;
-    next.disabled = false;
-   }
 }
