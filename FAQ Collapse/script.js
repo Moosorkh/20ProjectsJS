25fea0e6079b617606a3f3bfaae7507cb2406ed8
Fortis Irdmousa <moosorkh@gmail.com>
2022-09-25 00:43:48 -0700

    JS added, FAQ completed

---
+++ FAQ Collapse/script.js
@@ -0,0 +1,14 @@
+const faqs = document.querySelector('.faq');
+const buttons = document.querySelectorAll('.faq-toggle');
+
+
+  buttons.forEach(button=>{
+    button.addEventListener("click", () => {
+      button.parentNode.classList.toggle('active');
+    });
+  })
+  
+
+
+
+
