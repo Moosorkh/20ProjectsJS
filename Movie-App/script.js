1385189a64952880ef0b096148a7cb8e15cfb336
Fortis Irdmousa <moosorkh@gmail.com>
2023-11-05 14:37:27 -0800

    Movie app completed

--- Movie-App/script.js
+++ Movie-App/script.js
@@ -1,5 +1,7 @@
 //const { options } = require("pg/lib/defaults");
 
+
+const main = document.getElementById('main');
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=380f39b98db9dfc4c52917ac049d5420&page=1";
 
@@ -17,7 +19,42 @@ const search = document.getElementById("search");
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log(data.results);
+    showMovies(data.results);
+  }
+
+  function showMovies(movies){
+    main.innerHTML = '';
+
+    movies.forEach((movie)=>{
+        const {title, poster_path, vote_average, overview} = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+                
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>${title}</h3>
+                ${overview}
+            </div>
+        `;
+        main.appendChild(movieEl);
+    })
+  }
+
+  function getClassByRate(vote){
+    if(vote >= 8){
+        return 'green';
+    }else if(vote >= 5){
+        return 'orange';
+    }else{
+        return 'red';
+    }
   }
 
   getMovies(API_URL);
